aedb62d1fb06c4627708aecbf72e7bbf2059a418
Safak TURKELI <sfktrkl@gmail.com>
2021-12-15 04:53:04 +0300

    Clean the project for tutorial
    Setting up the application
    https://www.youtube.com/watch?v=9QQSPFtZ6Uk&list=PLaFzfwmPR7_LTXu0Vz9Zz_Y0OMMC7ArHZ&index=6
    
    Setting the repository
    https://www.youtube.com/watch?v=lQMiMyMQHVg&list=PLaFzfwmPR7_LTXu0Vz9Zz_Y0OMMC7ArHZ&index=7

--- Program.cs
+++ Program.cs
@@ -1,11 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Tutorial
 {
@@ -17,8 +11,7 @@ namespace Tutorial
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
