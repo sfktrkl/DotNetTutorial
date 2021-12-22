dbc2465a7d7a1567be876c920f6ebfdbfa0b43df
Safak TURKELI <sfktrkl@gmail.com>
2021-12-23 01:42:15 +0300

    Override existing html tag
    https://www.youtube.com/watch?v=6fgXaFux2ns&list=PLaFzfwmPR7_LTXu0Vz9Zz_Y0OMMC7ArHZ&index=64

---
+++ Helpers/BigTagHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Tutorial.Helpers
+{
+    // To be able to target any tag use this attribute.
+    // This is to create a big tag.
+    [HtmlTargetElement("big")]
+    // This is to create an attribute to a tag.
+    [HtmlTargetElement(Attributes = "big")]
+    // These to are separated because they have
+    // an 'or' like relationship.
+    public class BigTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            // Replace the tag name.
+            output.TagName = "h3";
+
+            // Remove the all big attributes already assigned.
+            output.Attributes.RemoveAll("big");
+
+            // Add an attribute to the tag.
+            output.Attributes.SetAttribute("class", "h3");
+        }
+    }
+}
