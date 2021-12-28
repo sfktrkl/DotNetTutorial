39c0ce16f913a62a76b636ee0883385acbf2407a
Safak TURKELI <sfktrkl@gmail.com>
2021-12-29 00:59:33 +0300

    Design a registration (Signup) page
    https://www.youtube.com/watch?v=pU2VaJaDhyE&list=PLaFzfwmPR7_LTXu0Vz9Zz_Y0OMMC7ArHZ&index=92

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tutorial.Models;
+
+namespace Tutorial.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("signup")]
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        [Route("signup")]
+        [HttpPost]
+        public IActionResult Signup(SignUpUserModel userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                ModelState.Clear();
+            }
+            return View();
+        }
+    }
+}
