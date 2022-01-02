5b863fcf53d0bfdc849fd6c92bbc6d4d96f11ac3
Safak TURKELI <sfktrkl@gmail.com>
2022-01-02 21:06:57 +0300

    Routing in areas
    https://www.youtube.com/watch?v=V4Gk1nNNEWU&list=PLaFzfwmPR7_LTXu0Vz9Zz_Y0OMMC7ArHZ&index=114

--- Areas/Admin/Controllers/HomeController.cs
+++ Areas/Admin/Controllers/HomeController.cs
@@ -4,15 +4,19 @@ namespace Tutorial.Areas.Admin.Controllers
 {
     // Define the name of the area
     [Area("admin")]
+    // Enable the routing
+    [Route("admin")]
     public class HomeController : Controller
     {
         // GET: HomeController
+        [Route("")]
         public ActionResult Index()
         {
             return View();
         }
 
         // GET: HomeController/Details/5
+        [Route("details/{id?}")]
         public ActionResult Details(int id)
         {
             return View(id);
